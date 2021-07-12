454bde630ce4d0e80a9251923d66e020a54530e3
Moonwar AL Wardiful <49252168+moonwarnishan@users.noreply.github.com>
2021-07-13 03:59:40 +0600

    simple bank

---
+++ script.js
@@ -0,0 +1,42 @@
+function buttonclick(){
+    document.getElementById("login-area").style.display="none";
+    document.getElementById("bal").style.display="block";
+}
+document.getElementById("login").addEventListener('click',buttonclick);
+
+
+function dep()
+{   
+    var depamount=parseFloat(document.getElementById('depamount').value);
+    if(depamount>0){
+        document.getElementById('balance').innerHTML=parseFloat(document.getElementById('balance').innerHTML)+depamount;
+        document.getElementById('deposit').innerText=parseFloat(document.getElementById('deposit').innerText)+depamount;        
+        document.getElementById('depamount').value="$ Enter amount to deposit";
+    }
+    else
+    {
+        document.getElementById("aaa").innerText="invalid amount and deposit amount must be above 0"
+    }
+
+}
+
+function wit()
+{
+    if(parseFloat(document.getElementById('balance').innerHTML)-parseFloat(document.getElementById('withamount').value)>0)
+    {
+        document.getElementById('balance').innerHTML=parseFloat(document.getElementById('balance').innerHTML)-parseFloat(document.getElementById('withamount').value);
+        document.getElementById('withdraw').innerText=parseFloat(document.getElementById('withdraw').innerText)+parseFloat(document.getElementById('withamount').value);
+        document.getElementById('withamount').value="$ Enter amount to withdraw";
+    }
+    else
+    {
+        document.getElementById("aaa").innerText="You dont have not enough balance"
+    }
+}
+
+document.getElementById('btn2').addEventListener('click',wit);
+document.getElementById('btn1').addEventListener('click',dep);
+
+
+
+
